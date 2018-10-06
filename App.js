e2e09d0e3f959a33e5e08490371e59e70ec404de
FizzyPM <munjal.pranav14@gmail.com>
2018-10-06 11:26:55 +0530

    Converted only React syntax to react-native

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, Button, ScrollView, Switch} from "react-native";
+
+let id = 0;
+const Todo = props => (
+	<View>
+    <Switch value={props.todo.checked} onValueChange={props.onPressed} />
+		<Button onPress={props.onDelete} title="delete" />
+		<Text>{props.todo.text}</Text>
+	</View>
+);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			todos: []
+		};
+	}
+	addTodo() {
+    id++
+    const text = `Todo number ${id}`
+		this.setState({
+			todos: [...this.state.todos, { id: id, text: text, checked: false }]
+		});
+	}
+	deleteTodo(id) {
+		this.setState({ todos: this.state.todos.filter(todo => todo.id !== id) });
+	}
+	toggleTodo(id) {
+		this.setState({
+			todos: this.state.todos.map(todo => {
+				if (todo.id !== id) return todo;
+				return {
+					id: todo.id,
+					text: todo.text,
+					checked: !todo.checked
+				};
+			})
+		});
+	}
+	render() {
+		return (
+			<View>
+				<Text>Total Todos: {this.state.todos.length} </Text>
+				<Text>
+					Unchecked Todos:{this.state.todos.filter(todo => todo.checked === false).length}
+				</Text>
+				<Button onPress={() => this.addTodo()} title="Add Todo" />
+				<ScrollView>
+					{this.state.todos.map(todo => (
+						<Todo
+							todo={todo}
+							onDelete={() => this.deleteTodo(todo.id)}
+							onPressed={() => this.toggleTodo(todo.id)}
+						/>
+					))}
+				</ScrollView>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
