9aa17963ebdb67ab44641d4aa6ea929b282886c8
FizzyPM <munjal.pranav14@gmail.com>
2018-10-10 05:44:20 +0530

    User input functionality

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {View, Text, Button, ScrollView, Switch} from "react-native";
+import {View, Text, Button, ScrollView, Switch, TextInput, Alert} from "react-native";
 import {Constants} from 'expo'
 
 let id = 0;
 const Todo = props => (
-	<View style={{ flexDirection: 'row', alignItems: 'center', flex:1 }}>
-    <Switch value={props.todo.checked} onValueChange={props.onPressed} />
-		<Button onPress={props.onDelete} title="delete" />
+	<View style={{ flexDirection: 'row', alignItems: 'center', flex:1, justifyContent:'space-between' }}>
+    	<Switch value={props.todo.checked} onValueChange={props.onPressed} />
 		<Text>{props.todo.text}</Text>
+		<Button onPress={props.onDelete} title="delete" />
 	</View>
 );
 
@@ -15,15 +15,20 @@ export default class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			todos: []
+			todos: [] ,
+			text : ''
 		};
 	}
 	addTodo() {
-    id++
-    const text = `Todo number ${id}`
+		if(this.state.text == ''){
+			Alert.alert('Please enter something!');
+			return
+		}
+		this.textInput.clear()
 		this.setState({
-			todos: [...this.state.todos, { id: id, text: text, checked: false }]
+			todos: [...this.state.todos, { id: id++, text: this.state.text, checked: false }]
 		});
+		this.setState({ text:'' })
 	}
 	deleteTodo(id) {
 		this.setState({ todos: this.state.todos.filter(todo => todo.id !== id) });
@@ -47,6 +52,12 @@ export default class App extends React.Component {
 				<Text>
 					Unchecked Todos:{this.state.todos.filter(todo => todo.checked === false).length}
 				</Text>
+				<TextInput
+					style={{height: 40}}
+					placeholder=""
+					ref={input => { this.textInput = input }}
+					onChangeText={(text) => this.setState({text})}
+        		/>
 				<Button onPress={() => this.addTodo()} title="Add Todo" />
 				<ScrollView style={{ flex:1 }}>
 					{this.state.todos.map(todo => (
